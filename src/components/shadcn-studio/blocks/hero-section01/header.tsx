afaefcd91731b76702c8faa14b35fe526c5f304f
Eduardo <eduardoteixeirapaulino777@gmail.com>
2025-10-28 14:05:57 -0300

    fix: hero layout

--- src/components/shadcn-studio/blocks/hero-section01/header.tsx
+++ src/components/shadcn-studio/blocks/hero-section01/header.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from '@/components/theme-toggle';
 
 export const SimpleHeader = () => {
   return (
-    <header className="sticky top-0 z-30 flex items-center justify-between px-4 lg:px-40 py-4 bg-background/90 backdrop-blur-md shadow-sm">
+    <header className="sticky top-0 z-30 flex items-center justify-between px-4 lg:px-40 py-4 bg-background/30 backdrop-blur-md shadow-sm">
       <Logo />
 
       <ModeToggle />
